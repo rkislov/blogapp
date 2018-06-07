9d24a09fd97184ca15c492dcba7fe51659896da4
Roman Kislov <roman@kislovs.ru>
2018-06-07 19:02:06 +0500

    Initial commit

---
+++ client/src/components/containers/News.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class News extends Component{
+    render() {
+        return (
+            <div>Список Новостей</div>
+        )
+    }
+}
+
+export default News
---
+++ client/src/components/layouts/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+        <div>Добро пожаловать</div>
+        )
+    }
+}
+
+export default Home
